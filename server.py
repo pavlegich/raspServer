1992602edc3d7f5bfd352249c82efc1c75c2baa7
pavlegich <p.gorodyansky@gmail.com>
2020-11-30 16:11:59 +0300

    time

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ class Camera(object):
         self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
     def get_frame(self):
-        return self.frames[int(time()) % 3]
+        return self.frames[int(time.time()) % 3]
 
 def gen(camera):
     while True:
