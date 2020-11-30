88745cc28f6c5a4186e6679adffa6847ff50ee53
pavlegich <p.gorodyansky@gmail.com>
2020-11-30 16:06:44 +0300

    frame

--- server.py
+++ server.py
@@ -25,6 +25,13 @@ endpoint = {
 
 # 'user' : auth.username()
 
+class Camera(object):
+    def __init__(self):
+        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+
+    def get_frame(self):
+        return self.frames[int(time()) % 3]
+
 def gen(camera):
     while True:
         frame = camera.get_frame()
