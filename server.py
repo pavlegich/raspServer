13a4d6f8514bdc48a9ac29de863ca115adc9a7bd
pavlegich <p.gorodyansky@gmail.com>
2020-12-01 16:19:38 +0300

    video

--- server.py
+++ server.py
@@ -23,11 +23,9 @@ endpoint = {
 # 'user' : auth.username()
 
 def gen(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
