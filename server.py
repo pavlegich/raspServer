4e6608babe4a31d62badbaae0c392bd7a55a5866
pavlegich <p.gorodyansky@gmail.com>
2020-12-01 16:27:10 +0300

    video

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_httpauth import HTTPDigestAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 import time
 import random
-from picamera import Camera
+from camera_pi import Camera
 
 
 
