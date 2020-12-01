29880c6a609f6e7c3490ec1ef3b30f4febd1dfa1
pavlegich <p.gorodyansky@gmail.com>
2020-12-01 16:15:58 +0300

    video

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import time
 import random
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hello'
 auth = HTTPDigestAuth()
@@ -20,9 +22,17 @@ endpoint = {
 
 # 'user' : auth.username()
 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/video_feed')
 def video_feed():
-    return render_template("video_feed.html")
+	def video_feed():
+		return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @auth.get_password
 def get_pw(username):
