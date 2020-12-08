03d5cae9a13d05fc1e417f6bcdd29d446d55cf78
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 13:19:23 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -2,15 +2,16 @@ import os
 from pymavlink import mavutil
 import time
 
-# master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
-master = mavutil.mavlink_connection('udp:0.0.0.0:14550')
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+# master = mavutil.mavlink_connection('udp:0.0.0.0:14550')
 
 master.wait_heartbeat()
 
 # Get some information !
 while True:
 	try:
-		print(master.recv_match().to_dict())
+		altitude = master.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
+		timestamp = master.time_since('GPS_RAW_INT')
+		print(altitude)
 	except:
-		pass
-	time.sleep(0.1)
+		print('No GPS_RAW_INT message received')
