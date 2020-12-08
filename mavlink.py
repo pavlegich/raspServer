4929a3afbb809e40c04f5399aa899cf7a26961a6
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 13:56:34 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -2,14 +2,13 @@ import os
 from pymavlink import mavutil
 import time
 
-# master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
-master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
-
-master.wait_heartbeat()
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+# master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
 # Get some information !
 while True:
 	try:
+		master.wait_heartbeat()
 		# lat = master.messages['GPS_RAW_INT'].lat*1e-7  # Note, you can access message fields as attributes!
 		# lon = master.messages['GPS_RAW_INT'].lon*1e-7
 		# alt = master.messages['GPS_RAW_INT'].alt*1e-3
