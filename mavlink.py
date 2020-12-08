e276b57eb132e1899f1359aa744a133e78b307e9
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 13:01:09 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -3,7 +3,7 @@ from pymavlink import mavutil
 import time
 
 # master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
-master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+master = mavutil.mavlink_connection('udpout:0.0.0.0:14550')
 
 master.wait_heartbeat()
 
