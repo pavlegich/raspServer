ed08db55bba2a05a7f18abfc1e1aada01b3868b3
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 13:50:50 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -2,22 +2,23 @@ import os
 from pymavlink import mavutil
 import time
 
-master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
-# master = mavutil.mavlink_connection('udp:0.0.0.0:14550')
+# master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
-master.wait_heartbeat()
+# master.wait_heartbeat()
 
 # Get some information !
 while True:
 	try:
-		lat = master.messages['GPS_RAW_INT'].lat*1e-7  # Note, you can access message fields as attributes!
-		lon = master.messages['GPS_RAW_INT'].lon*1e-7
-		alt = master.messages['GPS_RAW_INT'].alt*1e-3
-		latG = master.messages['GLOBAL_POSITION_INT'].lat*1e-7
-		print(lat)
-		print(lon)
-		print(alt)
-		print(latG)
+		# lat = master.messages['GPS_RAW_INT'].lat*1e-7  # Note, you can access message fields as attributes!
+		# lon = master.messages['GPS_RAW_INT'].lon*1e-7
+		# alt = master.messages['GPS_RAW_INT'].alt*1e-3
+		# latG = master.messages['GLOBAL_POSITION_INT'].lat*1e-7
+		# print(lat)
+		# print(lon)
+		# print(alt)
+		# print(latG)
+		print ("Global Location: %s" % vehicle.location.global_frame)
 	except:
 		print('No GPS_RAW_INT message received')
 	time.sleep(1)
