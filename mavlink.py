01e506d35fff8bb5fc9d2f95cc81d5cd5193450a
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 13:55:28 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -5,7 +5,7 @@ import time
 # master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
-# master.wait_heartbeat()
+master.wait_heartbeat()
 
 # Get some information !
 while True:
