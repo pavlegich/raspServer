0592b26126a46de6e75381e631afe75208612bd3
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 12:19:16 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -10,10 +10,10 @@ master.wait_heartbeat()
 # sudo mavproxy.py —master=/dev/ttyACM0,230400 —out=udpout:0.0.0.0:14550
 # sudo mavproxy.py —master=/dev/ttyACM0 —baudrate 921600 —aircraft MyCopter
 
-while True:
-	try:
-		altitude = master.messages['GPS_RAW_INT']  # Note, you can access message fields as attributes!
-		timestamp = master.time_since('GPS_RAW_INT')
-		print(altitude)
-    except:
-    	print('No GPS_RAW_INT message received')
+# while True:
+# 	try:
+# 		altitude = master.messages['GPS_RAW_INT']  # Note, you can access message fields as attributes!
+# 		timestamp = master.time_since('GPS_RAW_INT')
+# 		print(altitude)
+#     except:
+#     	print('No GPS_RAW_INT message received')
