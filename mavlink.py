8c5695e9a196c472bb939c44481ad2e0b05f6c97
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 12:20:49 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -7,10 +7,10 @@ master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
 master.wait_heartbeat()
 
-# while True:
-# 	try:
-# 		altitude = master.messages['GPS_RAW_INT']  # Note, you can access message fields as attributes!
-# 		timestamp = master.time_since('GPS_RAW_INT')
-# 		print(altitude)
-#     except:
-#     	print('No GPS_RAW_INT message received')
+while True:
+	try:
+		altitude = master.messages['GPS_RAW_INT']  # Note, you can access message fields as attributes!
+		timestamp = master.time_since('GPS_RAW_INT')
+		print(altitude)
+    except:
+    	print('No GPS_RAW_INT message received')
