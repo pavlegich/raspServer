14824a8294d97b3468b74279ae7a3222bd858e43
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 17:54:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -118,7 +118,7 @@ def status():
 		alt = vehicle.messages['GPS_RAW_INT'].alt*1e-3
 		if (lat == UAV['x'] and lon == UAV['y'] and alt == UAV['z']):
 			state = -1
-		elif (math.fabs(lat-UAV['x'])>0.00045 or math.fabs(lon-UAV['y'])>0.00045 or math.fabs(alt-UAV['z'])>2):
+		elif (math.fabs(lat-UAV['x'])>0.0002 or math.fabs(lon-UAV['y'])>0.0002 or math.fabs(alt-UAV['z'])>2):
 			state = 0
 		UAV['x'] = lat
 		UAV['y'] = lon
