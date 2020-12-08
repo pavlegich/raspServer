6f9c9225c984c9b4ac09b97ecc07a63a724672fb
pavlegich <p.gorodyansky@gmail.com>
2020-12-08 13:58:10 +0300

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -9,16 +9,12 @@ master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 while True:
 	try:
 		master.wait_heartbeat()
-		# lat = master.messages['GPS_RAW_INT'].lat*1e-7  # Note, you can access message fields as attributes!
-		# lon = master.messages['GPS_RAW_INT'].lon*1e-7
-		# alt = master.messages['GPS_RAW_INT'].alt*1e-3
-		# latG = master.messages['GLOBAL_POSITION_INT'].lat*1e-7
-		# print(lat)
-		# print(lon)
-		# print(alt)
-		# print(latG)
-		lat = master.messages['GPS_RAW_INT'].lat*1e-7
+		lat = master.messages['GPS_RAW_INT'].lat*1e-7  # Note, you can access message fields as attributes!
+		lon = master.messages['GPS_RAW_INT'].lon*1e-7
+		alt = master.messages['GPS_RAW_INT'].alt*1e-3
 		print(lat)
+		print(lon)
+		print(alt)
 	except:
 		print('No GPS_RAW_INT message received')
 	time.sleep(1)
