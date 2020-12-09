e8f5f6ed428082f3743f86535c9534c52f971ee6
pavlegich <p.gorodyansky@gmail.com>
2020-12-09 15:55:09 +0300

    Update arm_disarm.py

--- arm_disarm.py
+++ arm_disarm.py
@@ -2,9 +2,18 @@
 Example of how to Arm and Disarm an Autopilot with pymavlink
 """
 # Import mavutil
-from pymavlink import mavutil
+from pymavlink import mavutil, mavextra
 import time
 
 # Create the connection
 vehicle = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 # vehicle = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+
+while True:
+    vehicle.wait_heartbeat()
+    lat = vehicle.messages["GPS_RAW_INT"].lat*1e-7
+    lon = vehicle.messages["GPS_RAW_INT"].lon*1e-7
+    alt = vehicle.messages["GPS_RAW_INT"].alt*1e-3
+    new = vehicle.gps_newpos(lat, lon, 0, 10)
+    print(new)
+
